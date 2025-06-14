a5c91253298cd7f6e361e9f88f9365d05840db8d
Younes <101892216+VirtuMind@users.noreply.github.com>
2025-06-14 20:28:58 +0100

    init

---
+++ src/main/java/com/marketplace/catalogue/CatalogueApiApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueApiApplication.class, args);
+    }
+
+}
