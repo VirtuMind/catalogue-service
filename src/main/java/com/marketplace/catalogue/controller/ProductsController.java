11202339d4e1806245726806ee232ec1937d2218
Younes <101892216+VirtuMind@users.noreply.github.com>
2025-06-20 20:43:38 +0100

    Define project structure

---
+++ src/main/java/com/marketplace/catalogue/controller/ProductsController.java
@@ -0,0 +1,15 @@
+package com.marketplace.catalogue.controller;
+
+import com.marketplace.catalogue.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductsController {
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+}
